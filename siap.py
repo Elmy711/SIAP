80b0f29a1cf1da9cc3e11ca79c4b888da40640cb
my🫶el_711 <elmiya0711@gmail.com>
2026-07-18 17:17:21 +0700

    update

--- siap.py
+++ siap.py
@@ -56,7 +56,7 @@ def print_banner():
 =======================================================
 """ 
     print("\033[1;34m" + banner + "\033[0m")
-    print_banner()
+
     
 def test_url(url, method='HEAD', timeout=5):
     headers = {'User-Agent': random.choice(user_agents)}
